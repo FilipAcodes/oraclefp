e04aab516105cc6f583fd49783f502d0f4ce7a30
Filip Altankov <filipaltankov@hotmail.com>
2024-04-10 18:14:49 -0400

    api changes/frontend rendering

--- src/app/api/Read/route.js
+++ src/app/api/Read/route.js
@@ -1,3 +1,17 @@
+const oracledb = require("oracledb");
+import { NextResponse } from "next/server";
+
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+
 export async function GET() {
-  return new Response("hi");
+  let connection;
+  connection = await oracledb.getConnection({
+    user: "filip",
+    password: "filip",
+    connectionString: "//localhost:1521/global_oracle",
+  });
+  const data = await connection.execute(`SELECT * FROM EMPLOYEES`);
+  return NextResponse.json({
+    data: data.rows,
+  });
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,26 @@
+"use client";
+import { useState, useEffect } from "react";
 export default function Home() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    fetch("/api/Read")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.data);
+      });
+  }, []);
+  console.log(data);
   return (
     <main>
-      <h1>PH text</h1>
+      {data?.map((x) => {
+        return (
+          <div>
+            <p>{x.FIRST_NAME}</p>
+            <p>{x.LAST_NAME}</p>
+            <p>{x.DEPT_ID}</p>
+          </div>
+        );
+      })}
     </main>
   );
 }
