83c72a6e826170bd0e53e77d9b24d2cf0ebb9dc3
Filip Altankov <filipaltankov@hotmail.com>
2024-05-04 06:11:44 -0400

    customer page done with link to countries

--- src/app/customer/[id]/page.js
+++ src/app/customer/[id]/page.js
@@ -1,5 +1,66 @@
 "use client";
 import { useParams } from "next/navigation";
+import React from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import "../../table.css";
 export default function page() {
-  return <p>Post: {useParams().id}</p>;
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const useparams = useParams();
+  const router = useRouter();
+  useEffect(() => {
+    fetch(`/api/customer/${useparams.id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.data);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <main>
+      <h1>Customer</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Fname</th>
+            <th>Lname</th>
+            <th>Address</th>
+            <th>City</th>
+            <th>Country Id</th>
+            <th>Email</th>
+            <th>Phone #</th>
+            <th>Postal Code</th>
+            <th>Street Name</th>
+            <th>Street Number</th>
+            <th>Apartment Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((e) => {
+            return (
+              <tr key={e.ID}>
+                <td>{e.ID}</td>
+                <th>{e.FNAME}</th>
+                <th>{e.LNAME}</th>
+                <th>{e.ADDRESS}</th>
+                <th>{e.CITY}</th>
+                <th onClick={() => router.push(`/countries/${e.COUNTRY_ID}`)}>
+                  {e.COUNTRY_ID}
+                </th>
+                <th>{e.EMAIL}</th>
+                <th>{e.PHONENUMBER}</th>
+                <th>{e.POSTAL_CODE}</th>
+                <th>{e.STREET_NAME}</th>
+                <th>{e.STREET_NUMBER}</th>
+                <th>{e.APART_NUMBER}</th>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </main>
+  );
 }
--- src/app/customer/page.js
+++ src/app/customer/page.js
@@ -1,31 +1,63 @@
 "use client";
 import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import "../table.css";
 export default function Home() {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
-  // useEffect(() => {
-  //   fetch("/api/Read")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setData(data.data);
-  //       setLoading(false);
-  //     });
-  // }, []);
-  console.log(data);
+  const router = useRouter();
+  useEffect(() => {
+    fetch("/api/customer")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.data);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <main>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        data?.map((x) => (
-          <div key={x.EMPLOYEE_ID}>
-            <p>
-              First Name: {x.FIRST_NAME} Last Name:{x.LAST_NAME}
-            </p>
-            <p>Salary: {x.SALARY}$</p>
-          </div>
-        ))
-      )}
+      <h1>Customers</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Fname</th>
+            <th>Lname</th>
+            <th>Address</th>
+            <th>City</th>
+            <th>Country Id</th>
+            <th>Email</th>
+            <th>Phone #</th>
+            <th>Postal Code</th>
+            <th>Street Name</th>
+            <th>Street Number</th>
+            <th>Apartment Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((e) => {
+            return (
+              <tr key={e.ID}>
+                <td>{e.ID}</td>
+                <th>{e.FNAME}</th>
+                <th>{e.LNAME}</th>
+                <th>{e.ADDRESS}</th>
+                <th>{e.CITY}</th>
+                <th onClick={() => router.push(`/countries/${e.COUNTRY_ID}`)}>
+                  {e.COUNTRY_ID}
+                </th>
+                <th>{e.EMAIL}</th>
+                <th>{e.PHONENUMBER}</th>
+                <th>{e.POSTAL_CODE}</th>
+                <th>{e.STREET_NAME}</th>
+                <th>{e.STREET_NUMBER}</th>
+                <th>{e.APART_NUMBER}</th>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </main>
   );
 }
