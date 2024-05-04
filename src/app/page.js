ec645890abc0e23f4dabbb330419377e18accc82
Filip Altankov <filipaltankov@hotmail.com>
2024-05-04 19:25:47 -0400

    Homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,10 @@
 "use client";
-import { useState, useEffect } from "react";
+import picture from "../app/alpha_er.png";
+import "./table.css";
 export default function Home() {
-  const [data, setData] = useState();
-  const [loading, setLoading] = useState(true);
-  // useEffect(() => {
-  //   fetch("/api/Read")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setData(data.data);
-  //       setLoading(false);
-  //     });
-  // }, []);
-  console.log(data);
   return (
     <main>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        data?.map((x) => (
-          <div key={x.EMPLOYEE_ID}>
-            <p>
-              First Name: {x.FIRST_NAME} Last Name:{x.LAST_NAME}
-            </p>
-            <p>Salary: {x.SALARY}$</p>
-          </div>
-        ))
-      )}
+      <img src={picture.src} alt="er diagram"></img>
     </main>
   );
 }
